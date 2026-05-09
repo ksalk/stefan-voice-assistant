8646365228909d1e67a246351a3f6c1ab484edb2
Konrad Sałkowski <konrad.salkowski@gmail.com>
2026-05-09 14:31:45 +0200

    feat: add basic console app with HTTP server and background worker

---
+++ apps/node/Stefan.Node/Program.cs
@@ -0,0 +1,22 @@
+﻿var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.AddHostedService<Worker>();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "OK");
+
+await app.RunAsync("http://0.0.0.0:8080");
+
+public class Worker : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            // background logic
+
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
